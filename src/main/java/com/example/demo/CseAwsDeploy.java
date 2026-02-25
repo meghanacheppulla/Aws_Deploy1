f16981961cc99af5df31e4b790a19403b5b09204
23MH1A05F4 <meghanacheppulla@gmail.com>
2026-02-25 10:54:40 +0530

    Third commit

--- src/main/java/com/example/demo/CseAwsDeploy.java
+++ src/main/java/com/example/demo/CseAwsDeploy.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CseAwsDeploy {
-  @GetMapping("/home")   
-  		public String disk() {
-	  return "Welcome to CSE Jenkins";
-  }
-  @GetMapping("/home")   
-	  		public String disk2() {
-		  return "Welcome to CSE";
-	  }
+
+    @GetMapping("/home")
+    public String disk() {
+        return "Welcome to CSE Jenkins";
+    }
+
+    @GetMapping("/home1")
+    public String disk2() {
+        return "Welcome to CSE";
+    }
 }
